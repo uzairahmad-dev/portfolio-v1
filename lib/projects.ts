5f0f28462ad0f25576f61fdb175ca35be1a103d7
Uzair Ahmad <uzairahmad.dev@gmail.com>
2022-05-08 13:31:45 +0500

    change name of base url

--- lib/projects.ts
+++ lib/projects.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchProjects = async () => {
     try {
-        const response = await axios(`${process.env.BASE_URL}/api/projects`);
+        const response = await axios(`${process.env.NEXT_PUBLIC_SITE_URL}/api/projects`);
         if(response.data.length > 0) {
             return response.data;
         }
